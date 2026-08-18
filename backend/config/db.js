a972506a8c4504a2f550b30b056a0d3356eb6ab2
userazeeem <userazeeem@gmail.com>
2026-08-18 21:34:07 +0530

    Created DATABASE

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error("❌ MySQL connection failed:", err.message);
+        return;
+    }
+
+    console.log("✅ MySQL connected successfully!");
+});
+
+module.exports = connection;
